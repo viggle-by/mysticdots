3b81053492532c1ff3d431c15fdded9b74a9588f
viggle-by <barlevsj@gmail.com>
2025-10-29 17:37:48 -0400

    g

--- ins.sh
+++ ins.sh
@@ -4,5 +4,5 @@ yay -S --noconfirm --needed \
   fd eza fzf ripgrep zoxide bat jq \
   wl-clipboard fastfetch btop \
   man tldr less whois plocate bash-completion \
-  alacritty plymouth imagemagick grub-theme-minegrub-world-selection-git && git clone https://github.com/Davi-S/sddm-theme-minesddm.git ~/sddm-theme-minesddm && sudo cp -r ~/sddm-theme-minesddm/minesddm /usr/share/sddm/themes/ && git clone https://github.com/nikp123/minecraft-plymouth-theme cd minecraft-plymouth-theme && sudo ./install.sh && plymouth-set-default-theme -R mc
+  alacritty plymouth imagemagick && git clone https://github.com/Davi-S/sddm-theme-minesddm.git ~/sddm-theme-minesddm && sudo cp -r ~/sddm-theme-minesddm/minesddm /usr/share/sddm/themes/ && git clone https://github.com/nikp123/minecraft-plymouth-theme cd minecraft-plymouth-theme && sudo ./install.sh && plymouth-set-default-theme -R mc && git clone https://github.com/Lxtharia/minegrub-world-sel-theme.git && cd ./minegrub-world-sel-theme && sudo cp -ruv ./minegrub-world-selection /boot/grub/themes/
      
