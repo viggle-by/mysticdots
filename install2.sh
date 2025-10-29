9ab58e3a1ad091b0392bad50a548c64abc802536
viggle-by <barlevsj@gmail.com>
2025-10-29 10:56:00 -0400

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget -O /home/greengiggle/.config/fastfetch/config.jsonc https://raw.githubusercontent.com/mylinuxforwork/dotfiles/refs/heads/main/dotfiles/.config/fastfetch/config.jsonc
+wget -O /home/mysticgiggle/.config/fastfetch/config.jsonc https://raw.githubusercontent.com/mylinuxforwork/dotfiles/refs/heads/main/dotfiles/.config/fastfetch/config.jsonc
