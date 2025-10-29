20f9fd0909b15b9d79036e6d75e9987d93634dcc
viggle-by <barlevsj@gmail.com>
2025-10-29 10:29:45 -0400

    installer

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+git clone https://github.com/vinceliuice/MacTahoe-icon-theme && cd MacTahoe-icon-theme && ./install.sh
+git clone https://github.com/vinceliuice/MacTahoe-kde && cd MacTahoe-kde && ./install.sh
