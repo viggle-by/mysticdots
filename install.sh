f2534633f3b9d995f4dd53900be6fde9f97291de
viggle-by <barlevsj@gmail.com>
2025-10-30 16:24:28 -0400

    remove gex line

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 
 git clone https://github.com/vinceliuice/MacTahoe-icon-theme && cd MacTahoe-icon-theme && ./install.sh
 git clone https://github.com/vinceliuice/MacTahoe-kde && cd MacTahoe-kde && ./install.sh
-echo gex
 wget -O ~/.config/fastfetch/config.jsonc https://github.com/viggle-by/mysticdots/raw/refs/heads/main/config.jsonc
 git clone https://github.com/vinceliuice/ChromeOS-kde && cd ChromeOS-kde && ./install.sh
 git clone https://github.com/vinceliuice/Tela-circle-icon-theme && cd Tela-circle-icon-theme && ./install.sh
