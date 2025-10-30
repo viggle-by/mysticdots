e5a69854b4aa65233eb4a3daa1d5c748ab499db4
viggle-by <barlevsj@gmail.com>
2025-10-30 16:19:36 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@
 
 git clone https://github.com/vinceliuice/MacTahoe-icon-theme && cd MacTahoe-icon-theme && ./install.sh
 git clone https://github.com/vinceliuice/MacTahoe-kde && cd MacTahoe-kde && ./install.sh
+echo gex
